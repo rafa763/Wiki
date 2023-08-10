faed40a3353d564045d80a1c9cbb85aee07e58b2
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-08-10 12:53:46 +0300

    Entry page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from markdown2 import Markdown
 
-from . import util
-
+from . import util 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    entry = util.get_entry(title)
+    if entry is None:
+        return render(request, "encyclopedia/error.html", {
+            "title": title
+        })
+    html = Markdown().convert(entry)
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "content": html
+    })
