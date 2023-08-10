bef24cc220ca7ea06a266702cc6812d28b6c748e
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-08-10 18:52:43 +0300

    Create New Pages

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.entry, name="entry")
+    path("wiki/<str:title>", views.entry, name="entry"),
+    path("new", views.new, name="new"),
+    # path("edit/<str:title>", views.edit, name="edit"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.urls import reverse
 from markdown2 import Markdown
 
-from . import util 
+from . import util
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -13,10 +13,23 @@ def entry(request, title):
     entry = util.get_entry(title)
     if entry is None:
         return render(request, "encyclopedia/error.html", {
-            "title": title
+            "error_msg": f"Entry with title '{title}' does not exist."
         })
     html = Markdown().convert(entry)
     return render(request, "encyclopedia/entry.html", {
         "title": title,
         "content": html
     })
+
+def new(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        if util.get_entry(title) is None:
+            util.save_entry(title, content)
+            return redirect(reverse("encyclopedia:entry", args=[title]))
+        else:
+            return render(request, "encyclopedia/error.html", {
+                "error_msg": f"Entry with title '{title}' already exists."
+            })
+    return render(request, "encyclopedia/new.html")
